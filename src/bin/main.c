f95fd3aa00e8ba2df28c3c5f0b862a5d98f5f5e5
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-11-28 14:45:07 +1100

    ch2: Plumbing connected for 'select' and 'insert' statements

--- src/bin/main.c
+++ src/bin/main.c
@@ -20,6 +20,8 @@ InputBuffer *new_input_buffer() {
 }
 
 typedef enum {
+    META_EXIT_SUCCESS,
+    META_EXIT_FAILURE,
     META_CMD_SUCCESS,
     META_CMD_UNRECOGNISED_COMMAND,
 } MetaCmdResult;
@@ -31,6 +33,7 @@ typedef enum {
 
 typedef enum {
     STMT_INSERT,
+    STMT_SELECT,
 } StatementType;
 
 typedef struct {
@@ -57,14 +60,36 @@ static void read_input(InputBuffer *inbuf) {
 }
 
 MetaCmdResult do_meta_command(InputBuffer *inbuf) {
+    if (strcmp(inbuf->buf, ".exit") == 0) {
+        return META_EXIT_SUCCESS;
+    }
+
     return META_CMD_UNRECOGNISED_COMMAND;
 }
 
 PrepareResult prepare_statement(InputBuffer *inbuf, Statement *stmt) {
+    if (strncmp(inbuf->buf, "insert", 6) == 0) {
+        stmt->type = STMT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+
+    if (strcmp(inbuf->buf, "select") == 0) {
+        stmt->type = STMT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
     return PREPARE_UNRECOGNISED_STATEMENT;
 }
 
 void execute_statement(Statement *stmt) {
+    switch (stmt->type) {
+        case STMT_INSERT:
+            printf("INSERT occurs\n");
+            break;
+        case STMT_SELECT:
+            printf("SELECT occurs\n");
+            break;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -81,10 +106,16 @@ int main(int argc, char *argv[]) {
                 case META_CMD_UNRECOGNISED_COMMAND:
                     printf("Unrecognized command '%s'.\n", input_buffer->buf);
                     continue;
+                case META_EXIT_FAILURE:
+                    exit(EXIT_FAILURE);
+                    break;
+                case META_EXIT_SUCCESS:
+                    exit(EXIT_SUCCESS);
+                    break;
             }
         }
 
-        Statement stmt = { STMT_INSERT };
+        Statement stmt = {STMT_INSERT};
         switch (prepare_statement(input_buffer, &stmt)) {
             case PREPARE_SUCCESS:
                 break;
@@ -95,7 +126,6 @@ int main(int argc, char *argv[]) {
 
         execute_statement(&stmt);
         printf("Executed.\n");
-
     }
 
     return 0;
